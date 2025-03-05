d4bde2c51c9c07d5e193a0351a0435a1d802442a
ZachZQY <zhangquanyinhahaha@163.com>
2025-03-05 16:59:47 +0800

    Changes to be committed: 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
   poweredByHeader: false,
   // 如果您的站点不在域名根路径下，可以取消下面这行的注释并设置正确的基础路径
   // basePath: '',
+  async headers() {
+    return [
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
